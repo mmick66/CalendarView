44b04558dadb9c56cbd6255b72465486bc42e14e
Vitor mesquita <vitor.mesquita09@gmail.com>
2018-01-17 10:58:26 -0300

    fixinig Style access level again

--- KDCalendar/CalendarView/CalendarView.swift
+++ KDCalendar/CalendarView/CalendarView.swift
@@ -84,20 +84,20 @@ public class CalendarView: UIView {
             }
         }
         
-        static var cellColorDefault         = UIColor(white: 0.0, alpha: 0.1)
-        static var cellTextColorDefault     = UIColor.gray
-        static var cellTextColorToday       = UIColor.gray
-        static var cellColorToday           = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.3)
-        static var cellBorderColor          = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.8)
-        static var cellBorderWidth: CGFloat = 2.0
-        static var cellShape                = CellShapeOptions.bevel(4.0)
+        public static var cellColorDefault         = UIColor(white: 0.0, alpha: 0.1)
+        public static var cellTextColorDefault     = UIColor.gray
+        public static var cellTextColorToday       = UIColor.gray
+        public static var cellColorToday           = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.3)
+        public static var cellBorderColor          = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.8)
+        public static var cellBorderWidth: CGFloat = 2.0
+        public static var cellShape                = CellShapeOptions.bevel(4.0)
         
-        static var cellEventColor           = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.8)
+        public static var cellEventColor           = UIColor(red: 254.0/255.0, green: 73.0/255.0, blue: 64.0/255.0, alpha: 0.8)
         
-        static var headerFontName: String   = "Helvetica"
-        static var headerTextColor          = UIColor.gray
+        public static var headerFontName: String   = "Helvetica"
+        public static var headerTextColor          = UIColor.gray
         
-        static var headerHeight: CGFloat    = 80.0
+        public static var headerHeight: CGFloat    = 80.0
     }
 
     let cellReuseIdentifier = "CalendarDayCell"
